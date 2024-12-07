2f5fcc507973538c00ec262d21be6183fc9a7f98
Vlad-Stefan Harbuz <vlad@vlad.website>
2024-12-07 14:18:14 +0000

    add 128-bit types

--- src/common.h
+++ src/common.h
@@ -8,28 +8,34 @@ typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
+typedef __int128_t int128;
 typedef int8 i8;
 typedef int16 i16;
 typedef int32 i32;
 typedef int64 i64;
+typedef int128 i128;
 
 typedef int8 bool8;
 typedef int16 bool16;
 typedef int32 bool32;
 typedef int64 bool64;
+typedef int128 bool128;
 typedef bool8 b8;
 typedef bool16 b16;
 typedef bool32 b32;
 typedef bool64 b64;
+typedef bool128 b128;
 
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
+typedef __uint128_t uint128;
 typedef uint8 u8;
 typedef uint16 u16;
 typedef uint32 u32;
 typedef uint64 u64;
+typedef uint128 u128;
 
 typedef float float32;
 typedef double float64;
