65bd81dff260981b5311a7c70246eec3671b23b3
Vlad-Stefan Harbuz <vlad@vlad.website>
2024-12-15 21:56:34 +0000

    day 15: add part 2

--- src/15-2.c
+++ src/15-2.c
@@ -35,35 +35,109 @@ void print_grid(enum Tile grid[GRID_SIZE][GRID_SIZE], i32 grid_width, i32 grid_h
     eprintf("\n");
 }
 
-void move(
+bool move(
     enum Tile grid[GRID_SIZE][GRID_SIZE],
     i32 grid_width,
     i32 grid_height,
     i32 *x,
     i32 *y,
     i32 dx,
-    i32 dy
+    i32 dy,
+    bool dry_run,
+    bool came_from_friend
 ) {
-    if (
-        !is_in_bounds(*x, *y, grid_width, grid_height) ||
-        grid[*x][*y] == WALL ||
-        grid[*x][*y] == EMPTY ||
-        !is_in_bounds(*x + dx, *y + dy, grid_width, grid_height) ||
-        grid[*x + dx][*y + dy] == WALL
-    ) {
-        return;
+    eprintf("(x %d) (y %d) (came_from_friend %d) (dry_run %d)\n", *x, *y, came_from_friend, dry_run);
+    if (grid[*x][*y] == BOX_LEFT) {
+        assert(grid[*x + 1][*y] == BOX_RIGHT);
     }
-    i32 forward_x = *x + dx;
-    i32 forward_y = *y + dy;
-    move(grid, grid_width, grid_height,
-        &forward_x, &forward_y,
-        dx, dy);
-    if (grid[*x + dx][*y + dy] == EMPTY) {
+    if (grid[*x][*y] == BOX_RIGHT) {
+        assert(grid[*x - 1][*y] == BOX_LEFT);
+    }
+    if (!is_in_bounds(*x, *y, grid_width, grid_height)) {
+        return false;
+    }
+    if (grid[*x][*y] == EMPTY) {
+        return true;
+    }
+    if (
+        grid[*x][*y] == WALL ||
+        !is_in_bounds(*x + dx, *y + dy, grid_width, grid_height) ||
+        grid[*x + dx][*y + dy] == WALL ||
+        (
+            !came_from_friend && grid[*x][*y] == BOX_LEFT && (
+                !is_in_bounds(*x + 1 + dx, *y + dy, grid_width, grid_height) ||
+                grid[*x + 1 + dx][*y + dy] == WALL
+            )
+        ) ||
+        (
+            !came_from_friend && grid[*x][*y] == BOX_RIGHT && (
+                !is_in_bounds(*x - 1 + dx, *y + dy, grid_width, grid_height) ||
+                grid[*x - 1 + dx][*y + dy] == WALL
+            )
+        )
+    ) {
+        eprintf("invalid move\n");
+        return false;
+    }
+
+    bool bumpee_is_friend = false;
+    if (
+        (grid[*x][*y] == BOX_LEFT && dx == 1) ||
+        (grid[*x][*y] == BOX_RIGHT && dx == -1)
+    ) {
+        bumpee_is_friend = true;
+    }
+
+    i32 bumpee_x = *x + dx;
+    i32 bumpee_y = *y + dy;
+    bool did_move_bumpee = move(grid, grid_width, grid_height,
+        &bumpee_x, &bumpee_y,
+        dx, dy,
+        dry_run,
+        bumpee_is_friend);
+
+    bool did_move_friend;
+    i32 friend_x;
+    i32 friend_y = *y;
+    bool have_checkable_friend = (
+        !bumpee_is_friend && !came_from_friend && (grid[*x][*y] == BOX_LEFT || grid[*x][*y] == BOX_RIGHT)
+    );
+    if (have_checkable_friend) {
+        if (grid[*x][*y] == BOX_LEFT) {
+            friend_x = *x + 1;
+        }
+        if (grid[*x][*y] == BOX_RIGHT) {
+            friend_x = *x - 1;
+        }
+        did_move_friend = move(grid, grid_width, grid_height,
+            &friend_x, &friend_y,
+            dx, dy,
+            dry_run,
+            true);
+    }
+
+    if (dry_run) {
+        eprintf("success - (have_checkable_friend %d) (did_move_friend %d) (did_move_bumpee %d)\n",
+            have_checkable_friend, did_move_friend, did_move_bumpee);
+        return (!have_checkable_friend || did_move_friend) && did_move_bumpee;
+    } else {
+        if (have_checkable_friend && !did_move_friend) {
+            eprintf("could not move friend\n");
+            return false;
+        }
+        if (grid[*x + dx][*y + dy] != EMPTY) {
+            eprintf("space ahead not empty\n");
+            return false;
+        }
         grid[*x + dx][*y + dy] = grid[*x][*y];
         grid[*x][*y] = EMPTY;
         *x = *x + dx;
         *y = *y + dy;
+        eprintf("moved\n");
+        return true;
     }
+
+    assert(false);
 }
 
 int main() {
@@ -71,7 +145,7 @@ int main() {
     i32 grid_height = 0;
     enum Tile grid[GRID_SIZE][GRID_SIZE] = {0};
 
-    int f = open("data/15-test1", O_RDONLY);
+    int f = open("data/15", O_RDONLY);
     if (f == -1) {
         eprintf("Could not open file\n");
         exit(1);
@@ -117,27 +191,40 @@ int main() {
 
     print_grid(grid, grid_width, grid_height);
 
-    /* i32 t = 0; */
-    /* while (*curr) { */
-    /*     i32 dx = 0; */
-    /*     i32 dy = 0; */
-    /*     if (*curr == '<') { */
-    /*         dx = -1; */
-    /*     } else if (*curr == '>') { */
-    /*         dx = 1; */
-    /*     } else if (*curr == '^') { */
-    /*         dy = -1; */
-    /*     } else if (*curr == 'v') { */
-    /*         dy = 1; */
-    /*     } */
-    /*     if (dx != 0 || dy != 0) { */
-    /*         eprintf("t = %d, moving %c:\n", t, *curr); */
-    /*         /1* print_grid(grid, grid_width, grid_height); *1/ */
-    /*         move(grid, grid_width, grid_height, &x_robot, &y_robot, dx, dy); */
-    /*         t += 1; */
-    /*     } */
-    /*     curr += 1; */
-    /* } */
+    i32 t = 0;
+    while (*curr) {
+        i32 dx = 0;
+        i32 dy = 0;
+        if (*curr == '<') {
+            dx = -1;
+        } else if (*curr == '>') {
+            dx = 1;
+        } else if (*curr == '^') {
+            dy = -1;
+        } else if (*curr == 'v') {
+            dy = 1;
+        }
+        if (dx != 0 || dy != 0) {
+            eprintf("t = %d, moving %c:\n", t, *curr);
+            bool can_move = move(grid,
+                grid_width, grid_height,
+                &x_robot, &y_robot,
+                dx, dy,
+                true,
+                false);
+            if (can_move) {
+                move(grid,
+                    grid_width, grid_height,
+                    &x_robot, &y_robot,
+                    dx, dy,
+                    false,
+                    false);
+            }
+            /* print_grid(grid, grid_width, grid_height); */
+            t += 1;
+        }
+        curr += 1;
+    }
 
     i32 sum = 0;
     for (i32 y = 0; y < grid_height; y += 1) {
